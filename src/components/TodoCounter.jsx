7affc47307ee9b5ec7b55c3fdc6b31d87397c25e
molano5858 <molano5858@gmail.com>
2024-01-05 16:22:57 -0500

    Making dinamic classes when todo is completed or not, adding styles to the components

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -2,7 +2,7 @@ import './TodoCounter.css'
 
 function TodoCounter ({completed,total}){
     return (
-        <h1 className='TodoCounter'>Has completado {completed} de {total} To Do´s</h1>
+        <h1 className='TodoCounter'>Has completado <span>{completed}</span> de <span>{total}</span> To Do´s</h1>
     )
 }
 
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,9 +3,9 @@ import './TodoItem.css'
 function TodoItem ({todoName,completed}){
     return (
         <li className='TodoItem'>
-            <span>V</span>
-            <p>{todoName}</p>
-            <span>X</span>
+            <span className={`Icon Icon-check ${completed ? 'Icon-check--active':null }`}>V</span>
+            <p className={`TodoItem-p ${completed?'TodoItem-p--complete':null}`}>{todoName}</p>
+            <span className='Icon Icon-delete'>X</span>
             {console.log(todoName,completed)}
         </li>
     )
