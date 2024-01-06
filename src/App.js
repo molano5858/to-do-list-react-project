716c4be3e7abfd5bc5dacbba3a1a6856ae984b6e
molano5858 <molano5858@gmail.com>
2024-01-06 15:33:30 -0500

    Creating search functionality, it works with lower and upper case

--- src/App.js
+++ src/App.js
@@ -1,5 +1,63 @@
+// import './App.css';
+// import React, {useState} from 'react';
+// import { CreateTodoButton } from './components/CreateTodoButton';
+// import { TodoCounter } from './components/TodoCounter';
+// import { TodoItem } from './components/TodoItem';
+// import { TodoList } from './components/TodoList';
+// import { TodoSearch } from './components/TodoSearch';
+
+// const defaultTodos=[
+//   {text:'Tarea 1', completed:true},
+//   {text:'Tarea 2', completed:true},
+//   {text:'Tarea 3', completed:false},
+//   {text:'Tarea 4', completed:true},
+//   {text:'Tarea 5', completed:false},
+// ]
+
+// function App() {
+//   const [todos,setTodos]=useState(defaultTodos)
+//   const [searchValue,setSearchValue]=useState('')
+
+//   const completedTodos=defaultTodos.filter((todo)=>todo.completed===true).length;
+//   const totalTodos=todos.length
+
+//   const searchedTodos= todos.filter((todo)=>todo.text.includes(searchValue))
+//   console.log(searchedTodos)
+
+//   function searchHandler (event){
+//     const text=event.target.value
+//     setSearchValue(text)
+//   }
+
+//   return (
+//     <div className="App">
+//       <TodoCounter completed={completedTodos} total={totalTodos}/>
+//       <TodoSearch 
+//         searchValue={searchValue}
+//         setSearchValue={setSearchValue}
+//         searchHandler={searchHandler}
+//       />
+//       <TodoList>
+//         {
+//          todos.map((todo)=>{
+//           return  < TodoItem key={todo.text} todoName={todo.text} completed={todo.completed}/>
+//         }) 
+//         }
+//       </TodoList>
+//       <CreateTodoButton />
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// --------------------------------------------------
+
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import { CreateTodoButton } from './components/CreateTodoButton';
 import { TodoCounter } from './components/TodoCounter';
 import { TodoItem } from './components/TodoItem';
@@ -7,14 +65,12 @@ import { TodoList } from './components/TodoList';
 import { TodoSearch } from './components/TodoSearch';
 
 const defaultTodos=[
-  {text:'Tarea 1', completed:true},
-  {text:'Tarea 2', completed:true},
-  {text:'Tarea 3', completed:false},
-  {text:'Tarea 4', completed:true},
-  {text:'Tarea 5', completed:false},
+  {text:'felipe', completed:true},
+  {text:'paola', completed:true},
+  {text:'matias', completed:false},
+  {text:'Terry', completed:true},
+  {text:'Dante', completed:false},
 ]
-
-
 
 function App() {
   const [todos,setTodos]=useState(defaultTodos)
@@ -22,6 +78,9 @@ function App() {
 
   const completedTodos=defaultTodos.filter((todo)=>todo.completed===true).length;
   const totalTodos=todos.length
+
+  const searchedTodos= todos.filter((todo)=>todo.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()))
+  console.log(searchedTodos)
 
   function searchHandler (event){
     const text=event.target.value
@@ -38,7 +97,7 @@ function App() {
       />
       <TodoList>
         {
-         todos.map((todo)=>{
+         searchedTodos.map((todo)=>{
           return  < TodoItem key={todo.text} todoName={todo.text} completed={todo.completed}/>
         }) 
         }
@@ -50,3 +109,4 @@ function App() {
 }
 
 export default App;
+
