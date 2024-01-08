e57aab724bac3265cdc0022afeb68ada140d16c8
molano5858 <molano5858@gmail.com>
2024-01-08 13:44:51 -0500

    search todo functionality and check completed functionality

--- src/App.js
+++ src/App.js
@@ -1,61 +1,3 @@
-// import './App.css';
-// import React, {useState} from 'react';
-// import { CreateTodoButton } from './components/CreateTodoButton';
-// import { TodoCounter } from './components/TodoCounter';
-// import { TodoItem } from './components/TodoItem';
-// import { TodoList } from './components/TodoList';
-// import { TodoSearch } from './components/TodoSearch';
-
-// const defaultTodos=[
-//   {text:'Tarea 1', completed:true},
-//   {text:'Tarea 2', completed:true},
-//   {text:'Tarea 3', completed:false},
-//   {text:'Tarea 4', completed:true},
-//   {text:'Tarea 5', completed:false},
-// ]
-
-// function App() {
-//   const [todos,setTodos]=useState(defaultTodos)
-//   const [searchValue,setSearchValue]=useState('')
-
-//   const completedTodos=defaultTodos.filter((todo)=>todo.completed===true).length;
-//   const totalTodos=todos.length
-
-//   const searchedTodos= todos.filter((todo)=>todo.text.includes(searchValue))
-//   console.log(searchedTodos)
-
-//   function searchHandler (event){
-//     const text=event.target.value
-//     setSearchValue(text)
-//   }
-
-//   return (
-//     <div className="App">
-//       <TodoCounter completed={completedTodos} total={totalTodos}/>
-//       <TodoSearch 
-//         searchValue={searchValue}
-//         setSearchValue={setSearchValue}
-//         searchHandler={searchHandler}
-//       />
-//       <TodoList>
-//         {
-//          todos.map((todo)=>{
-//           return  < TodoItem key={todo.text} todoName={todo.text} completed={todo.completed}/>
-//         }) 
-//         }
-//       </TodoList>
-//       <CreateTodoButton />
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-// --------------------------------------------------
-
 import './App.css';
 import React, {useState,useEffect} from 'react';
 import { CreateTodoButton } from './components/CreateTodoButton';
@@ -79,14 +21,25 @@ function App() {
   const completedTodos=defaultTodos.filter((todo)=>todo.completed===true).length;
   const totalTodos=todos.length
 
-  const searchedTodos= todos.filter((todo)=>todo.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()))
-  console.log(searchedTodos)
+  const searchedTodos= todos.filter((todo)=>{
+    const textToSearch=todo.text.toLocaleLowerCase();
+    const todoText=searchValue.toLocaleLowerCase();
+    return (textToSearch.includes(todoText))
+    }
+  )
 
   function searchHandler (event){
     const text=event.target.value
     setSearchValue(text)
   }
 
+  function completeTodo(todoName){
+    const newTodos=[...todos]
+    const todoNameIndex= newTodos.findIndex((todo)=>todo.text===todoName)
+    newTodos[todoNameIndex].completed=!newTodos[todoNameIndex].completed
+    setTodos(newTodos)
+  } 
+ 
   return (
     <div className="App">
       <TodoCounter completed={completedTodos} total={totalTodos}/>
@@ -98,8 +51,12 @@ function App() {
       <TodoList>
         {
          searchedTodos.map((todo)=>{
-          return  < TodoItem key={todo.text} todoName={todo.text} completed={todo.completed}/>
-        }) 
+          return  < TodoItem 
+            key={todo.text} 
+            todoName={todo.text} 
+            completed={todo.completed} 
+            completeTodo={()=>(completeTodo(todo.text))}/>
+          }) 
         }
       </TodoList>
       <CreateTodoButton />
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,17 @@
 import './TodoItem.css'
 
-function TodoItem ({todoName,completed}){
+function TodoItem ({todoName,completed,completeTodo}){
+    
+
+    function deleteTodoHandler(){
+        console.log('tarea ELIMINADA')
+    }
+
     return (
         <li className='TodoItem'>
-            <span className={`Icon Icon-check ${completed ? 'Icon-check--active':null }`}>V</span>
+            <span onClick={completeTodo} className={`Icon Icon-check ${completed ? 'Icon-check--active':null }`}>V</span>
             <p className={`TodoItem-p ${completed?'TodoItem-p--complete':null}`}>{todoName}</p>
-            <span className='Icon Icon-delete'>X</span>
+            <span className='Icon Icon-delete' onClick={deleteTodoHandler}>X</span>
         </li>
     )
 }
